ba12236f221096fb3a7c1313a96c5ae0900cb5b1
Andre-Almeida <andre.almeida@sigmavaf.com.br>
2025-09-11 16:25:41 -0300

    sla

--- src/Controller/ProdutoController.php
+++ src/Controller/ProdutoController.php
@@ -1,20 +1,36 @@
 <?php
-
 namespace App\Controller;
 
+use App\Form\ProdutoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProdutoController extends AbstractController
 {
     /**
-     * @Route("/produto", name="app_produto")
+     * @Route("/produto/novo", name="produto_novo")
      */
-    public function index(): Response
+    public function novo(Request $request): Response
     {
-        return $this->render('produto/index.html.twig', [
-            'controller_name' => 'ProdutoController',
+        $form = $this->createForm(ProdutoType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $pdo = new \PDO("mysql:host=localhost;bd_teste;charset=utf8", "andre", "abc123!");
+            $stmt = $pdo->prepare("INSERT INTO produto (nameProduto, preco, estoque) VALUES (:nameProduto, :preco, :estoque)");
+            $stmt->bindValue(':nameProduto', $data['nameProduto']);
+            $stmt->bindValue(':preco', $data['preco']);
+            $stmt->bindValue(':estoque', $data['estoque']);
+            $stmt->execute();
+        }
+
+        return $this->render('produto/novo.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
